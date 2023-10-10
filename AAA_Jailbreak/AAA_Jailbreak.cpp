21d2b02614115b974248dcc2f2f852133b2729eb
Nour Elsayyid <nourelsayyid@gmail.com>
2023-10-09 21:45:57 -0400

    Made a function for the backstory, dsiplayed game title and game objective

--- AAA_Jailbreak/AAA_Jailbreak.cpp
+++ AAA_Jailbreak/AAA_Jailbreak.cpp
@@ -3,18 +3,42 @@
 
 #include <iostream>
 
-int main()
+using namespace std;
+
+
+string printBackstory() // backstory function. Can call it when the user inputs "story"
 {
-    std::cout << "Hello World from backstory branch visible in Jira!\n";
+    //backstory
+    string backStory;
+    backStory =  "In the realm of tax consulting, you're a shining star, renowned for your prowess in deciphering the complex labyrinth of tax codes."
+        " Your standing in the community is unblemished, but fate takes a dark turn when your neighbor, burdened by his ailing wife's medical bills,"
+        "approaches you with a drastic proposal: to dance on the edge of legality and commit tax fraud. "
+        "Brimming with integrity, you bravely decline, opting for the high road.Undeterred, your neighbor ventures forth, successfully securing the much" 
+        "- needed loan.However, this triumph is short - lived as the IRS sniffs out irregularities and turns the spotlight on him. Cornered, he deflects attention,"
+        "naming you as his trusted tax advisor, leaving you in the crosshairs. Soon, you're summoned for a gripping interrogation, the gravity of the situation "
+        "hanging heavy in the air. The legal battle that ensues is nothing short of a high-stakes drama, where your attempts to set the record straight fall on deaf ears."
+        "Against all odds, you find yourself on a rollercoaster ride through the justice system, ultimately facing a daunting 15-year sentence in a federal prison. "
+        "The tale of your unwavering moral compass and the fight for justice paints a riveting portrait of an unexpected journey through the twists and turns of the law. \n";
+
+    return backStory;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main()
+{
+    //Displaying Game title
+    cout << "-------Jailbreak-------\n";
+    //displaying backstory
+    printBackstory(); // need help with getting this to display - Nour
+    //Displaying game objective
+    cout << "Game Objective: Go through different tasks to escape the prison in the shortest amount of days possible."
+        "You will have items and people to help you get out along the way.   \n";
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    //Need a message letting the user know that if they type "story", they can get the backstory again
+
+    // need to make a conditional statement to redisplay the backstory when user inputs "story"
+    /* if user types == "story"
+    *   printBackStory();
+    */
+
+}
+
